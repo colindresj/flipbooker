44636d5ebadde10bb54c100e45fb79a2df5d3eed
JC <jorge.e.colindres@gmail.com>
2014-01-20 10:10:14 -0500

    add more tests

--- test/flipbooker_test.js
+++ test/flipbooker_test.js
@@ -3,28 +3,60 @@
   module('jQuery.flipbooker', {
     setup: function() {
       this.arr = [
-        {href: "http://placehold.it/350x151", pause: 500},
-        {href: "http://placehold.it/350x152", caption: "hi"},
-        {href: "http://placehold.it/350x153"},
-        {href: "http://placehold.it/350x154", pause: 500},
-        {href: "http://placehold.it/350x155"},
-        {href: "http://placehold.it/350x156", caption: "hi"},
-        {href: "http://placehold.it/350x157"},
-        {href: "http://placehold.it/350x158"},
-        {href: "http://placehold.it/350x159"}
+        {href: "http://placehold.it/350x151", caption: 'test', pause: 500},
+        {href: "http://placehold.it/350x152", caption: 'test'},
+        {href: "http://placehold.it/350x153", pause: 500},
+        {href: "http://placehold.it/350x154"}
       ];
+
+      this.$container = $('#qunit-fixture');
     }
   });
 
   test('ovrrides the defaults with options', function() {
     expect(2);
     $.flipbooker(this.arr, {
-        container: '#qunit-fixture'
-      });
+      container: '#qunit-fixture'
+    });
 
-    ok($('#qunit-fixture').children('img'), 'overriden container has an image element');
-    ok($('#qunit-fixture').children('div'), 'overriden container has a div element');
+    ok(this.$container.children('img'), 'overriden container has an image element');
+    ok(this.$container.children('div'), 'overriden container has a div element');
   });
+
+  test('adds the correct styles to the container', function() {
+    expect(4);
+    $.flipbooker(this.arr, {
+      container: '#qunit-fixture'
+    });
+
+    equal(this.$container.height(), 400, 'the container has the correct height');
+    equal(this.$container.width(), 400, 'the container has the correct width');
+    equal(this.$container.css('position'), 'relative', 'the container has relative position' );
+    equal(this.$container.css('overflow'), 'hidden', 'the container has overflow hidden' );
+  });
+
+  asyncTest('it fires the callback', function() {
+    expect(1);
+    $.flipbooker(this.arr, {
+      container: '#qunit-fixture',
+      cb: function(){
+        ok(true, 'callback fired');
+        start();
+      }
+    });
+  });
+  // End jQuery.flipbooker
+
+  module('events');
+
+  test('it calls the flipbooker events', function() {
+    expect(1);
+    $(window).on('pause:flipbooker', function(){
+      ok(true, 'event triggered');
+    });
+    $.flipbooker.pause();
+  });
+  // End events
 
 
 }(jQuery));
